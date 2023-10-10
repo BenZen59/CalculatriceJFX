2e8a4c67336a82140cd99ba08dbdc9efd19fd28a
BenZen59 <benoit.zenone@gmail.com>
2023-10-10 13:34:55 +0200

    add css

--- src/main/java/fr/fs/calculatrice/CalculatriceApplication.java
+++ src/main/java/fr/fs/calculatrice/CalculatriceApplication.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,11 +23,12 @@ public class CalculatriceApplication extends Application {
         stage.setTitle("Calculatrice");
         stage.setScene(scene);
         stage.setMinWidth(300);
-        stage.setMinHeight(430);
+        stage.setMinHeight(440);
         stage.setMaxWidth(900);
         stage.setMaxHeight(1000);
         stage.setWidth(380);
         stage.setHeight(450);
+        stage.getIcons().add(new Image("file:src/main/resources/images/calculator.png"));
         widthChangeListener = (observable, oldValue, newValue) -> widthChange(newValue);
         heightChangeListener = (observable, oldValue, newValue) -> heightChangeListener(newValue);
         stage.widthProperty().addListener(widthChangeListener);
@@ -36,13 +38,13 @@ public class CalculatriceApplication extends Application {
 
     private void widthChange(Number newValue) {
         stage.heightProperty().removeListener(heightChangeListener);
-        stage.setHeight(newValue.doubleValue() / 1.5);
+        stage.setHeight(newValue.doubleValue() * 1.5);
         stage.heightProperty().addListener(heightChangeListener);
     }
 
     private void heightChangeListener(Number newValue) {
         stage.widthProperty().removeListener(widthChangeListener);
-        stage.setWidth(newValue.doubleValue() * 1.5);
+        stage.setWidth(newValue.doubleValue() / 1.5);
         stage.widthProperty().addListener(widthChangeListener);
     }
 
